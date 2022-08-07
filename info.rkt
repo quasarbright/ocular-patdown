bf2d9c039ed4170b6e12c22391570f20fbca3f74
Mike Delmonaco <mthundershock@gmail.com>
2022-08-07 18:02:54 -0400

    add syntax-classes-lib dependency

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "pattern-updating")
-(define deps '("base"))
+(define deps '("base" "syntax-classes-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/pattern-updating.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "match-like syntax for deep immutable updates")
 (define version "0.0")
 (define pkg-authors '(mdelmonaco))
