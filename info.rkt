4b72a397fb355502b5b67c8e1faedf6a93948092
Mike Delmonaco <mthundershock@gmail.com>
2022-08-22 19:52:56 -0400

    ocular-patdown

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
-(define collection "pattern-updating")
-(define deps '("base" "syntax-classes-lib"))
+(define collection "ocular-patdown")
+(define deps '("base" "syntax-classes-lib" "https://github.com/michaelballantyne/bindingspec.git#b1e6f3c42556e898e983f1b39595c02357a0406c"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/pattern-updating.scrbl" ())))
 (define pkg-desc "match-like syntax for deep immutable updates")
---
+++ scribblings/ocular-patdown.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[ocular-patdown
+                    racket/base]]
+
+@title{ocular-patdown}
+@author{mdelmonaco}
+
+@defmodule[ocular-patdown]
+
+pattern matching, but you can do immutable updates via optics.
