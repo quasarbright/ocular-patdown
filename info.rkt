93ba29daa81d54d563d76cf89964d912bb0f4af7
Mike Delmonaco <mthundershock@gmail.com>
2022-08-25 15:11:12 -0400

    point bindingspec package spec to github

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "ocular-patdown")
-(define deps '("base" "syntax-classes-lib" "bindingspec"))
+(define deps '("base" "syntax-classes-lib" "https://github.com/michaelballantyne/bindingspec.git"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/pattern-updating.scrbl" ())))
 (define pkg-desc "match-like syntax for deep immutable updates")
