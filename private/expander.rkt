7101d74c0509335eb9bb5e915720c2bf5ae68dc6
Mike Delmonaco <mthundershock@gmail.com>
2022-09-06 15:29:53 -0400

    tests from old compiler pass

--- private/expander.rkt
+++ private/expander.rkt
@@ -74,16 +74,15 @@
 (define-pattern-syntax struct-field
   (syntax-parser [(_ struct-name:struct-id field-name:id (~optional field-pat #:defaults ([field-pat #'field-name])))
                   (define/syntax-parse predicate (get-struct-pred-id #'struct-name))
-                  #'(optic predicate (struct-field-lens struct-name field-name) field-pat)]))
-(define-pattern-syntax iso (syntax-rules () [(iso target? forward backward pat) (optic target? (make-iso forward-backward) pat)]))
+                  #'(optic predicate (struct-lens struct-name field-name) field-pat)]))
+(define-pattern-syntax iso (syntax-rules () [(iso target? forward backward pat) (optic target? (make-iso forward backward) pat)]))
 (define-pattern-syntax and (syntax-rules () [(and) _] [(and p0 p ...) (and2 p0 (and p ...))]))
 (define-pattern-syntax ? (syntax-rules () [(? predicate p ...) (and (#%? predicate) p ...)]))
 
 (define-syntax update
   (syntax-parser
-    [(_ target [pat body] clause ...)
-     ; begin might need to be (let () body ...)
-     #'(update* target pat body (update target clause ...))]
+    [(_ target [pat body ...] clause ...)
+     #'(update* target pat (let () body ...) (update target clause ...))]
     [(_ target)
      #'(error 'update "no matching clause for ~v" target)]))
 
@@ -168,8 +167,94 @@
   (define (compile-host-expr e) (resume-host-expansion e #:reference-compilers ([var compile-reference]))))
 
 (module+ test
+  ; you can set values
+  (check-equal? (update (list 1 2) [(list a b) (set a #t) (set b #f)])
+                (list #t #f))
+  ; you can apply functions to modify values
+  (check-equal? (update (list 1 2) [(list a b) (modify b add1)])
+                '(1 3))
+  ; you can nest patterns to perform deep updates
+  (check-equal? (update '(1 (2 3)) [(list a (list b c)) (set c #t)])
+                '(1 (2 #t)))
+  ; get, set, etc. are procedures and can be used in any expression position inside of an update form
+  (check-equal? (update (list 7 8 9)
+                        [(list a b c)
+                         (set a 1)
+                         (set b (get a))
+                         (define x (get c))
+                         (set c (add1 x))])
+                (list 1 1 10))
+  ; struct pattern
+  (struct posn [x y] #:transparent)
+  (check-equal? (update (posn 1 2) [(struct-field posn x a) (set a 3)]) (posn 3 2))
+  ; without a pattern after the field name, the field's optic is bound to the name of the field
+  (check-equal? (update (posn 1 2) [(struct-field posn x) (set x 3)]) (posn 3 2))
+  ; list-of pattern creates a traversal which can modify all elements
+  (check-equal? (update (list 1 2 3 4) [(list-of a) (modify a -)]) '(-1 -2 -3 -4))
+  ; you can fold(l) the elements of a traversal
+  (check-equal? (update (list 1 2 3) [(list-of a) (fold a cons '())])
+                (list 3 2 1))
+  ; if the last expression of the body is a get, its value is returned
+  (check-equal? (update '(1 2) [(list a b) (get a)]) 1)
+  ; you can access the optic directly
+  (check-pred lens? (update '(1 2) [(list a b) a]))
+  (test-equal? "can use iso to treat an X as a Y"
+               (update 'foo [(iso symbol? symbol->string string->symbol str) (modify str string-upcase)])
+               'FOO)
+  (test-equal? "can specify an optic directly"
+               (update (list 1 2) [(optic list? list-traversal a) (modify a -)])
+               (list -1 -2))
+  (test-equal? "and pattern"
+               (update (list 1 2) [(and2 (list-of n) (list a b))
+                                   (modify n -)
+                                   (modify a number->string)])
+               (list "-1" -2))
+  (test-equal? "? pattern"
+               (update (list 1 2) [(list (and2 (? odd?) a) (? even?)) (modify a -)])
+               (list -1 2))
+  (test-equal? "multi-clause"
+               (update (list 1 2)
+                       [(struct-field posn x) (set x 3)]
+                       [(list a b) (set a 4)])
+               (list 4 2))
+  (test-equal? "use current-update-target"
+               (update (list 1 2) [(list a b) (optic-set a (current-update-target) 3)])
+               '(3 2))
+  (check-equal? (update (list 1 2) [(cons a (cons b _)) (set a #t) (set b #f)])
+                (list #t #f))
+  (check-equal? (update (list 1 2) [(list a b) (modify a -) (set b #t)]) (list -1 #t))
+  (check-equal? (update (posn (cons 1 2) 3) [(struct-field posn x (cons a b)) (modify a -) (modify b sqr)]) (posn (cons -1 4) 3))
+  (check-equal? (update '((1 2) (3 4) (5 6)) [(list-of (list a _)) (modify a -)]) '((-1 2) (-3 4) (-5 6)))
+  (check-equal? (update '(((1 2) (3)) ((4) ())) [(list-of (list-of (list-of a))) (modify a -)]) '(((-1 -2) (-3)) ((-4) ())))
+  (test-equal? "update in an update works"
+               (update '(1 (2 3)) [(list a b)
+                                    (set b (update (get b) [(list c d) (set d 4)]))
+                                    (set a #t)])
+                '(#t (2 4)))
+  (test-equal? "iso composes"
+               (update '(foo bar) [(cons (iso symbol? symbol->string string->symbol str) _) (modify str string-upcase)])
+               '(FOO bar))
+  ; regression test:
+  ; structure check was broken for list-of. structure check only really worked for lenses.
+  ; it used to run the nested structure check on the list itself, not each element.
+  (test-equal? "list-of structs works"
+               (update (list (posn 1 2) (posn 3 4)) [(list-of (struct-field posn x)) (modify x -)])
+               (list (posn -1 2) (posn -3 4)))
+  (test-equal? "failure in a list-of"
+               (update (list 1 2 (posn 3 4))
+                       [(list-of (? number?)) 2]
+                       [_ 3])
+               3)
   (check-equal? (update 1 [_ 2]) 2)
   (check-pred optic? (update 1 [a a]))
   (check-equal? (update '(1) [(optic cons? car-lens a) (get a)]) 1)
   (check-equal? (update '(1 2) [(and a (cons b c)) (map get (list a b c))]) '((1 2) 1 (2)))
-  (check-equal? (update '(1 2) [(list a b) (get b)]) 2))
+  (check-equal? (update '(1 2) [(list a b) (get b)]) 2)
+  (test-equal? "multi-clause"
+                (update '(1 2)
+                        [(list a b c) (error "boom")]
+                        [(list a b) (list (get b) (get a))])
+                '(2 1))
+  (test-equal? "implicit begin in clause"
+               (update 1 [a (define x (get a)) x])
+               1))
